8a6595014649621c640505b2548811843e243eb5
croxis <croxis@yahoo.com>
2009-09-03 14:17:38 -0700

    Just need multithreaded socket system

--- CityMania/Server/server.py
+++ CityMania/Server/server.py
@@ -32,7 +32,6 @@ class EventManager(object):
         """
         self.listeners: {event: {object1: [method, [arguments]], object2: [method2, [arguments]]}, event2... }
         """
-        self.eventQueue = []
         self.listeners = {}
         
     def accept(self, event, object, method, extraArgs):
@@ -43,25 +42,74 @@ class EventManager(object):
             self.listeners[event] = {}
         self.listeners[event][object] = [method, extraArgs]
             
-    def send(self, event):
+    def send(self, event, extraArgs=[]):
         """
         Notify all listening object of an event
-        We will want to make this more intelligent to make the event system less chatty
+        TODO: Add error checking
         """
-        self.eventQueue.append(event)
-        
-    def tick(self):
-        """
-        Processes self.eventQueue, one event at a time.  
-        If no event, an empty tick event is sent (may not be necessary)
-        """
-        if self.eventQueue:
-            event = self.eventQueue.pop()
-        else:
-            event = Event()
-            
-        for listener in self.listeners:
-            listener.notify(event)
+        objects = self.listeners[event]
+        for object in objects:
+            method, extraArgs = objects[object]
+            method(extraArgs)
 
 # We initialize the CityMania engine
 messenger = EventManager()
+import time
+
+class Spinner(Entity):
+    """
+    Master game loop spinner thing
+    May wish to consider intigrating a task manager
+    Speeds are normal, medium, and fast speeds in Hz
+    Regulates simulation speed
+    Pause is set to high number to prevent divide by 0 errors
+    TODO: Develop system for Hz to be adjuststed based on slower clients
+    """
+    PAUSE = 999999999999999999.0
+    SLOW = 1.0
+    MEDIUM = 2.0
+    FAST = 3.0
+    def __init__(self):
+        self.running = False
+        self.speed = SLOW
+    
+    def start(self):
+        self.running = True
+        while self.running:
+            try:
+                # Possible problems: we might run into wierd dependency issues
+                # Also, this blank event is the time keeper tick, do we want this 
+                # inherient for all Entities, or just those worried about keeping
+                # track of the time?
+                self.step()
+                time.sleep(1/self.speed)
+            except KeyboardInterrupt:
+                self.stop()
+    
+    def stop(self):
+        self.running = False
+    
+    def step(self):
+        messenger.send(Event())
+    
+    def setSpeed(self, speed):
+        """
+        sets speed of simulation
+        speed = 0, 1, 2, 3
+        """
+        if speed is 0:
+            self.speed = PAUSE
+        elif speed is 1:
+            self.speed = SLOW
+        elif speed is 2:
+            self.speed = MEDIUM
+        elif speed is 3:
+            self.speed = FAST
+    
+spinner = Spinner()
+
+main():
+    pass
+
+if __name__ == "__main__":
+    main()
